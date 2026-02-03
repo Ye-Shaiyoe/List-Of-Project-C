d7f0b3f4435637d0a580bde5a7346de5dc8e11c8
Akram <akrommuhammadyusuf@gmail.com>
2026-02-03 21:05:45 +0700

    Add files via upload
    Signed-off-by: Akram <akrommuhammadyusuf@gmail.com>

---
+++ fastfetch.pro/include/colors.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define C_RESET "\033[0m"
+#define C_RED   "\033[31m"
+#define C_GREEN "\033[32m"
+#define C_YELLOW "\033[33m"
+#define C_BLUE  "\033[34m"
+#define C_CYAN  "\033[36m"
+#define C_WHITE "\033[37m"
+#define C_INTEL   "\033[34m"
+#define C_NVIDIA  "\033[32m"
+#define C_AMD     "\033[31m"
