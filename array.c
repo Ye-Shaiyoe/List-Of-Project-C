648497bc29ae606eeba48401b8c52d7f0589c08a
Akram <akrommuhammadyusuf@gmail.com>
2026-03-11 23:38:10 +0700

    Update array.c
    Signed-off-by: Akram <akrommuhammadyusuf@gmail.com>

--- array.c
+++ array.c
@@ -4,7 +4,7 @@ int main() {
     double number[6]= {20, 10, 20.2, 43.2, 54.0, 3.14159};
     char letters= 'A';
 
-    double sum = number[0] + number[1] + number[2] + number[3] + number[4] + number[5];
+    double sum = number[0] + number[1] + number[2] + number[3] + number[4] + number[5] + number[6];
 
     printf("the sum of % .2f, %.2f, %.2f, %.2f, %.2f and %.5f is %.5f\n",number[0], number[1], number[2], number[3], number[4], number[5], sum, letters);
     return 0;
